f63379aac2f8388d65b2fe7fbb35aa0dafe13cac
robsli <robbieli121.zy@gmail.com>
2018-05-16 22:27:09 -0500

    fix to CORS issue

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express()
 
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Credentials", true)
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000, http://journal-app.surge.sh')
+  res.setHeader('Access-Control-Allow-Origin', 'http://journal-app.surge.sh')
   res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, credentials")
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
   next()
